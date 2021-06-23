14ae74fb5f24490dad5fc2b16bc1c359aa28bcf2
kartik-alt <73581266+kartik-alt@users.noreply.github.com>
2021-06-23 16:30:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+nosex=0;
+nosey=0;
+rightwristx=0;
+leftwristx=0;
+diffrence=0;
+function setup (){
+canvas=createCanvas(500,500);
+canvas.position(800,150);
+video=createCapture(VIDEO);
+video.size(500,500);
+video.position(30,100)
+pn=ml5.poseNet(video,modelLoaded);
+pn.on('pose',gotPoses);
+
+}
+
+function draw(){
+  background("#969A97");
+fill("red")
+square(nosex, nosey,diffrence);
+
+}
+function modelLoaded(){
+    console.log("modelLoaded");
+}
+
+function gotPoses(results){
+    if(results.length>0){
+      console.log(results);
+      nosex=results[0].pose.nose.x;
+      nosey=results[0].pose.nose.y;
+      console.log("nosex= "+nosex+"nosey= "+nosey);
+      rightwristx= results[0].pose.rightWrist.x;
+      leftwristx= results[0].pose.leftWrist.x;
+      diffrence= floor(leftwristx-rightwristx);
+      console.log(" leftwrist= "+leftwristx+"rightwristx= "+rightwristx+"diffrence= "+diffrence);
+    }
+}
